2184b00c2e75a14e8a3fb809508d6b7965e9caac
noah <noahwoodward2244@gmail.com>
2019-04-20 21:57:39 -0700

    Items can be deleted

--- MOB_2.1_Tutorial_Starter_App/loaner/controllers/ViewController.swift
+++ MOB_2.1_Tutorial_Starter_App/loaner/controllers/ViewController.swift
@@ -54,8 +54,16 @@ class ViewController: UIViewController {
     }
     
     func deleteItem(at index: Int) {
+        // Delete the user-selected item from the context
+        let viewContext = store.persistentContainer.viewContext
+        viewContext.delete(items[index])
+        
+        // Delete the user-selected item from the data source
         items.remove(at: index)
         collectionView.deleteItems(at: [IndexPath(row: index, section: 0)])
+        
+        // Save changes to the Managed Object Context
+        store.saveContext()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
