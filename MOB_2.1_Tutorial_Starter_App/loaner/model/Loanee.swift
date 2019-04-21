21cf6adca43f63bb7efc9fb6cece70376af2dd8c
noah <noahwoodward2244@gmail.com>
2019-04-20 20:28:11 -0700

    Created entity and attributes

--- MOB_2.1_Tutorial_Starter_App/loaner/model/Loanee.swift
+++ MOB_2.1_Tutorial_Starter_App/loaner/model/Loanee.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 
-class Loanee: NSObject {
-
-        var name: String
-        var contactNumber: String?
+class Loanee: NSObject, NSCoding {
+    
+    
+    
+    var name: String
+    var contactNumber: String?
+    
+    enum Keys: String{
+        case name = "name"
+        case contactNumber = "contactNumber"
+    }
+    
+    init(name: String, contactNumber: String?) {
         
-        init(name: String, contactNumber: String?) {
-            
-            /** For Future Feature: Ability to access Contacts app:
-             init(name: String, profileImage: UIImage, contactNumber: String?) {
-             
-             self.profileImage = profileImage
-             **/
-            
-            self.name = name
-            self.contactNumber = contactNumber
-        }
+        /** For Future Feature: Ability to access Contacts app:
+         init(name: String, profileImage: UIImage, contactNumber: String?) {
+         
+         self.profileImage = profileImage
+         **/
+        
+        self.name = name
+        self.contactNumber = contactNumber
+    }
+    
+    
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(contactNumber, forKey: "contactNumber")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        contactNumber = aDecoder.decodeObject(forKey: "contactNumber") as? String
+        super.init()
+    }
+    
+    
 }
